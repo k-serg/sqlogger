fd2e94ec124bef4572362f6a61b7c1163c3d0768
Sergey K <ksv@shipbuilders.ru>
2025-03-10 00:15:11 +0300

    Add (c) notes.

--- include/log_strings.h
+++ include/log_strings.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of SQLogger.
+ *
+ * SQLogger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SQLogger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SQLogger. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 Sergey K. sergey[no_spam]@greenblit.com
+ */
+
 #ifndef LOG_STRINGS_H
 #define LOG_STRINGS_H
 
